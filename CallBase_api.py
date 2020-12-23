2c46219ff9d280882a2b1c6a8f68d21b1f774e5c
sophie22 <rzsozso@gmail.com>
2020-12-23 13:09:36 +0000

    init

---
+++ CallBase_api.py
@@ -0,0 +1,48 @@
+import json
+import requests
+
+def build_url(variant: list):
+    """ Builds external url path with parameters
+
+    Args:
+        variant in the format of Chrom:Pos:Ref:Alt
+
+    Returns:
+        url: URL for the API call
+    """
+
+    # if len(variant_list) > 1:
+    #     variant_list = ",".join(variant_list)
+    # else:
+    #     variant_list = variant_list[0]
+
+    ext_url = "%3A".join(variant.split(":"))
+    url = "http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest/v4/hsapiens/genomic/variant/{}/annotation?assembly=grch37&include=populationFrequencies&limit=-1&skip=-1&skipCount=false&count=false&Output%20format=json&normalize=false&phased=false&useCache=false&imprecise=false&svExtraPadding=0&cnvExtraPadding=0".format(ext_url)
+
+    return url
+
+def get_response(url: str):
+    """ Make an API query
+
+        Args:
+            url (str): Full url to use for the API call. Defaults to None.
+
+        Returns:
+            dict: Data from the API call
+    """
+    for i in range(0, 5):
+        try:
+            request = requests.get(url, headers={"Accept": "application/json"})
+        except Exception as e:
+            print("Something went wrong: {}".format(e))
+        else:
+            if request.ok:
+                data = json.loads(request.content.decode("utf-8"))
+                if len(data["response"][0]["result"]) > 0:
+                    return data["response"][0]["result"][0]
+                else:
+                #     print("Error {} for URL: {}".format(data["errors"], url))
+                    return None
+            else:
+                print("Error {} for URL: {}".format(request.status_code, url))
+                return None
---
+++ annotMAFcsv.py
@@ -0,0 +1,61 @@
+#vcf parser 3
+import sys
+import vcf
+import pandas as pd
+from CallBase_api import build_url, get_response
+
+g2t_list = []
+with open("g2tr.txt", 'r') as f:
+    for line in f:
+        g2t_list.append(line.strip().split('\t')[1])
+# print(g2t_list[:10])
+
+inp = sys.argv[1]
+variants = pd.DataFrame(columns=["chromosome", "position", "ref", "alt", "MAF", "HGVS", "HGNC", "RefSeq", "consequence", "other"])
+vcf_reader = vcf.Reader(open(inp, 'r'))
+
+def transcript_check(csq_list):
+    """
+    check if any of the transcripts annotated for the variant
+    are in the GEMINI genes2transcript list
+    """
+    # csq_transcripts = [i.split('|')[3] for i in csq_list]
+    found = False
+    for i in csq_list:
+        csq = i.split('|')
+        if csq[3] in g2t_list:
+            found = True
+            return [csq[1],csq[2],csq[3],csq[5],[""]] # "HGVS", "HGNC", "RefSeq", "consequence", "other"
+    if found is False:
+        csq_transcripts = [j.split('|')[3] for j in csq_list]
+        NM_transcripts = [k for k in csq_transcripts if k.startswith("NM_")]
+        return ["","","","",NM_transcripts]
+
+for record in vcf_reader:
+    # record = next(vcf_reader)
+    chrom = str(record.CHROM)
+    pos = str(record.POS)
+    ref = record.REF
+    alt = str(record.ALT[0])
+    v = ":".join([chrom,pos,ref,alt])
+    url = build_url(v)
+    data_result = get_response(url)
+    try:
+        popFreqStudies = data_result["populationFrequencies"]
+        gnEall = [i["altAlleleFreq"] for i in popFreqStudies if i["study"] == "GNOMAD_EXOMES" and i["population"] == "ALL"]
+        if gnEall[0] < 1:
+            csq_list = record.INFO['CSQ']
+            check = transcript_check(csq_list) # "HGVS", "HGNC", "RefSeq", "consequence", "other"
+            variants = variants.append({
+                "chromosome": chrom, "position": pos, "ref": ref, "alt": alt,
+                "MAF": gnEall[0], "HGVS": check[0], "HGNC": check[1],
+                "RefSeq": check[2], "consequence": check[3], "other": check[4]
+            }, ignore_index=True)
+
+    except:
+        pass
+        # print("") #data not available
+
+# print(variants.head(15))
+variants.to_csv("X111923_annot_MAF_filtered.tsv", sep='\t', encoding='utf-8', index=False)
+print("Done!")
